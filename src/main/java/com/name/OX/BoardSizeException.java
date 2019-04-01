0d70a3dc994f6c5d11126f35fd5dac880e26565d
sehta7@gmail.com <sehta7@gmail.com>
2019-04-01 20:13:07 +0200

    board size exception handle

---
+++ src/main/java/com/name/OX/BoardSizeException.java
@@ -0,0 +1,11 @@
+package com.name.OX;
+
+/**
+ * @author Ola Podorska
+ */
+class BoardSizeException extends Exception{
+
+    public BoardSizeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/name/OX/InputDataReader.java
+++ src/main/java/com/name/OX/InputDataReader.java
@@ -15,7 +15,7 @@ class InputDataReader {
 
     String readLanguage() {
         while (true) {
-            String language = "";
+            String language;
             try {
                 language = scanner.nextLine();
                 if (!AvailableLanguage.compareLanguage(language)) {
@@ -23,6 +23,7 @@ class InputDataReader {
                 }
                 return language;
             } catch (NotAvailableLanguageException e) {
+                //TODO: add displayer with question
                 language = readLanguage();
             }
             return language;
@@ -34,10 +35,34 @@ class InputDataReader {
     }
 
     String readBoardSize() {
-        String size = scanner.nextLine();
-        size += ",";
-        size += scanner.nextLine();
-        return size;
+        while (true) {
+            String size;
+            try {
+                size = scanner.nextLine();
+                size += ",";
+                size += scanner.nextLine();
+                if(!checkIfSizeIsGood(size)){
+                    throw new BoardSizeException("Board must be grater than 3 and lower than 100");
+                }
+                return size;
+            } catch (BoardSizeException e) {
+                //TODO: add displayer with question
+                size = readBoardSize();
+            } catch (NumberFormatException e){
+                //TODO: add displayer message (only number)
+                size = readBoardSize();
+            }
+            return size;
+        }
+    }
+
+    private boolean checkIfSizeIsGood(String size) {
+        String[] splitSize = size.split(",");
+        if (Integer.valueOf(splitSize[0]) < 3 || Integer.valueOf(splitSize[0]) > 100
+                || Integer.valueOf(splitSize[1]) < 3 || Integer.valueOf(splitSize[1]) > 100){
+            return false;
+        }
+        return true;
     }
 
     String readWinningSigns() {
