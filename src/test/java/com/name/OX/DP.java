ef1aad2a80f3dc782ff2173a178a68940cccd234
Ola <aleksandra.podorska@gmail.com>
2019-04-02 11:16:07 +0200

    test InputDataReader class

--- src/test/java/com/name/OX/DP.java
+++ src/test/java/com/name/OX/DP.java
@@ -29,9 +29,75 @@ class DP {
         };
     }
 
+    @DataProvider
+    public static Object[][] availableLanguage(){
+        return new Object[][]{
+                //TODO: iterate over AvailableLanguage enum
+                {"pl"},
+                {"en"}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] player(){
+        return new Object[][]{
+                //TODO: iterate over AvailableLanguage enum
+                {randomString()},
+                {randomString()},
+                {randomString()},
+                {randomString()},
+                {randomString()},
+                {randomString()},
+                {randomString()},
+                {randomString()}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] notAvailableLanguage(){
+        return new Object[][]{
+                {randomString(), "pl"},
+                {randomString(), "pl"},
+                {randomString(), "pl"},
+                {randomString(), "pl"},
+                {randomString(), "en"},
+                {randomString(), "en"},
+                {randomString(), "en"},
+                {randomString(), "en"}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] winningSigns(){
+        return new Object[][]{
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()},
+                {randomNumber()}
+        };
+    }
+
     private static String randomNumber() {
         Random random = new Random();
         int number = random.nextInt((MAX - MIN) + 1) + MIN;
         return String.valueOf(number);
+    }
+
+    private static String randomString(){
+        Random random = new Random();
+        int firstASCIIChar = 32;
+        int lastASCIIChar = 126;
+        StringBuilder randomString = new StringBuilder();
+        for (int i = 0; i < random.nextInt(15); i++){
+            char ascii = (char) (random.nextInt((lastASCIIChar - firstASCIIChar) + 1) + firstASCIIChar);
+            randomString.append(ascii);
+        }
+        return randomString.toString();
     }
 }
